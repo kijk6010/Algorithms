7b8715f02d7d99c8254eefb7ebbe349c7ed566e3
kijk6010 <50351960+kijk6010@users.noreply.github.com>
2019-10-09 21:10:41 +0900

    Update 이상한 문자 만들기.java

--- programmers/이상한 문자 만들기.java
+++ programmers/이상한 문자 만들기.java
@@ -1,15 +1,18 @@
 class Solution {
   public String solution(String s) {
-      String[] arr = s.split("");
+      char ch =0;
       String answer = "";
       int n=0;
-      for(int i=0;i<arr.length;i++){
-          if(arr[i]==" "){ n=1; }
+      for(int i=0;i<s.length();i++){
+          if(s.charAt(i)==' '){ 
+              n=1; 
+              ch = s.charAt(i);
+          }
           if(n%2==0){
-              arr[i]=arr[i].toUpperCase();
+              ch = Character.toUpperCase(s.charAt(i));
            }
-          else{ arr[i]=arr[i].toLowerCase();}
-          answer+=arr[i];
+          else{ ch = Character.toLowerCase(s.charAt(i));}
+          answer+=ch;
           n++;
       }
       return answer;
