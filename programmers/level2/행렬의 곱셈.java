67cc24cc5877296aeb6d0df213fd0e9569e3d4e9
kijk6010 <50351960+kijk6010@users.noreply.github.com>
2019-11-23 13:59:31 +0900

    Create 행렬의 곱셈.java

---
+++ programmers/level2/행렬의 곱셈.java
@@ -0,0 +1,13 @@
+class Solution {
+    public int[][] solution(int[][] arr1, int[][] arr2) {
+        int[][] answer = new int[arr1.length][arr2[0].length];
+        for(int i=0;i<arr2[0].length;i++){
+            for(int j=0;j<arr1.length;j++){
+                for(int k=0;k<arr2.length;k++){
+                    answer[j][i]+=arr1[j][k]*arr2[k][i];
+                }
+            }
+        }
+        return answer;
+    }
+}
